1c3bd1d7c5fd10b7f4c4231e0d4732f87a78489d
sharmaigne <71376956+sharmaigne@users.noreply.github.com>
2023-06-18 09:14:30 +0800

    moved validation.js to assets

--- insert.php
+++ insert.php
@@ -101,7 +101,7 @@
         </div>
     </form>
 
-    <script src="validation.js"></script>
+    <script src="assets/validation.js"></script>
 
     <!-- JQUERY, POPPER, BOOTSTRAP SCRIPTS -->
     <script src="https://code.jquery.com/jquery-3.2.1.slim.min.js" integrity="sha384-KJ3o2DKtIkvYIK3UENzmM7KCkRr/rE9/Qpg6aAZGJwFDMVNA/GpGFF93hXpG5KkN" crossorigin="anonymous"></script>
