14ec77b1455de48e628e9d85c9064aef2a72b4b0
sharmaigne <71376956+sharmaigne@users.noreply.github.com>
2023-06-18 18:44:02 +0800

    added support for all the other search boxes (outside admin options)

--- autocomplete.php
+++ autocomplete.php
@@ -2,12 +2,19 @@
 <?php require_once 'config.php'?>
 
 <?php
-    $query = "SELECT game_name, game_id FROM `market`";
+    $query = "SELECT game_name, game_id FROM `market` ORDER BY game_name ASC";
+    if ( isset($_GET['library']) || isset($_GET['library-search'])) {
+        $query = "SELECT game_name FROM `market` WHERE owned=1 ORDER BY game_name ASC";
+    }
     $result = mysqli_query($conn, $query);
 
     // generate datalist options
-    while ($row = mysqli_fetch_assoc($result)) {
-        echo '<option value="' . $row['game_name'] . ' #' . $row['game_id'] .'"></option>';
+    if (isset($_GET['index']) || isset($_GET['search']) || isset($_GET['user']) || isset($_GET['user-search']) || isset($_GET['library']) || isset($_GET['library-search'])) {
+        while ($row = mysqli_fetch_assoc($result))
+            echo '<option value="' . $row['game_name'] . '"></option>';
+    } else { // has gameId, for admin options
+        while ($row = mysqli_fetch_assoc($result))
+            echo '<option value="' . $row['game_name'] . ' #' . $row['game_id'] .'"></option>';
     }
 
 ?>
