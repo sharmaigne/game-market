b47cd0a29f74e0cf05bc9e8bb38b8a2a1ad1c55e
sharmaigne <71376956+sharmaigne@users.noreply.github.com>
2023-06-18 18:54:32 +0800

    added autocomplete support for search

--- navbar.php
+++ navbar.php
@@ -1,6 +1,6 @@
 <nav class="navbar navbar-expand-lg navbar-dark bg-dark">
     <div class="container">
-        <a class="navbar-brand" href="index.php">E&S</a>
+        <a class="navbar-brand" href="index.php?index=true">E&S</a>
         <ul class="navbar-nav mr-auto">
             <li class="nav-item dropdown">
                 <a href="#" role="button" class="nav-link dropdown-toggle" id="updateDropdown" data-bs-toggle="dropdown" aria-haspopup="true" aria-expanded="false">Admin Options</a>
@@ -12,12 +12,15 @@
             </li>
         </ul>
         <!-- Search bar -->
-        <form action="gallery.php" method = "GET" class="d-flex"> 
-            <input class="form-control me-2" placeholder="Search games..." aria-label="Search" id="Search" name="search" type="text">
-            <button class="btn btn-outline-light" type="submit"><i class="bi bi-search"></i></button>
+        <form action="gallery.php" method="GET" class="d-flex">
+            <input list="gameSuggestions" class="form-control me-2" placeholder="Search games..." aria-label="Search" id="Search" name="search" type="text">
+            <datalist id="gameSuggestions" class="search-datalist">
+                <?php include_once 'autocomplete.php';?>
+            </datalist>
+            <button class="btn btn-outline-light" type="submit"> <i class="bi bi-search"></i> </button>
         </form>
         
         <!-- View as User button -->
-        <a class="btn btn-light" href="#" role="button">View as User</a>
+        <a class="btn btn-light" href="user.php?user=true" role="button">View as User</a>
     </div>
 </nav>
